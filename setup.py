b4c98dd00c7ed128bff8230f61fd386cbcdbfac2
SelmiAbderrahim <codingwithselmi@gmail.com>
2022-07-24 12:41:58 +0100

    😔 change name to easy-py-selenium

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='easy selenium',
+    name='easy py selenium',
     version=__version__,
     author='Selmi Abderrahim',
     author_email='contact@selmi.tech',
